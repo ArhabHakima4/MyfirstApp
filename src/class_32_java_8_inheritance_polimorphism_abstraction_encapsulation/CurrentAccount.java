3d146c844db07dd49e6aea92b8665104edf2ab2f
ArhabHakima4 <arhabhakima4@gmail.com>
2024-09-09 19:00:35 -0500

    adding cuurrent account

---
+++ src/class_32_java_8_inheritance_polimorphism_abstraction_encapsulation/CurrentAccount.java
@@ -0,0 +1,20 @@
+package class_32_java_8_inheritance_polimorphism_abstraction_encapsulation;
+//subclass: CurentAccount (inheritance & method Overriding
+public class CurrentAccount extends BankAccount{
+    private final double overdraftLimit = 1000.0;
+    //constructor
+    public CurrentAccount (String accountHolderName, int accountNumber, double balance) {
+        super (accountHolderName, accountNumber, balance);
+    }
+
+
+    //override withdraw method to allow overdraft
+    @Override
+    public void withdraw(double amount) {
+        if (getBalance() + overdraftLimit >= amount) {
+            System.out.println("withdrawn: $"+amount+" from Current Account");
+        }else {
+            System.out.println("withdrawal denied. Overdraft limit exceeded");
+        }
+    }
+}
